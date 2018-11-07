422030e185bf3ccba0bacd8be94ed9ba0a8ea84c
Orta Therox <orta.therox@gmail.com>
2018-11-07 18:29:40 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # mondrian
 A/B Testing as a Service
+
+### Getting set up
+
+```sh
+git clone https://github.com/orta/mondrian.git
+cd mondrian
+
+# Install the AWS CLI
+brew bundle
+```
---
+++ graphql.js
@@ -0,0 +1,28 @@
+const { ApolloServer, gql } = require('apollo-server-lambda');
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+ 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ event, context }) => ({
+    headers: event.headers,
+    functionName: context.functionName,
+    event,
+    context,
+  }),
+});
+
+exports.handler = server.createHandler();
